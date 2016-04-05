02b67d1c9f9c0b3e5a2b7ea0929a82bca7ffbf00
Joey Stoffels <joey.stoffels@gmail.com>
2016-04-05 14:31:54 +0200

    moon

--- src/demo/HelloWorld.java
+++ src/demo/HelloWorld.java
@@ -1,8 +1,12 @@
 package demo;
 
 public class HelloWorld {
+	
 	public static void main(String[] args) {
 		System.out.println("Hallo Wereld! nog een keer dan maar");
+	
 		
 	}
+	
+	void getMoon(){}
 }
