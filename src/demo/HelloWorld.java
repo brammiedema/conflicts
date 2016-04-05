a4fc35ee09b011e53f8342321ebe891cef33d488
Joey Stoffels <joey.stoffels@gmail.com>
2016-04-05 14:35:45 +0200

    bla

--- src/demo/HelloWorld.java
+++ src/demo/HelloWorld.java
@@ -2,13 +2,10 @@ package demo;
 
 public class HelloWorld {
 	
-<<<<<<< HEAD
 	
 	
-=======
 	int dirkjanhaan;
 
->>>>>>> branch 'master' of https://github.com/brammiedema/conflicts.git
 	public static void main(String[] args) {
 		System.out.println("Hallo Wereld! nog een keer dan maar");
 	
