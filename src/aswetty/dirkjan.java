f0de44af7999ba21bc6bda7096f765ce1a9acc65
Cesar Chretien <cesarchre10@gmail.com>
2016-04-05 14:54:15 +0200

    drikjanned

--- src/aswetty/dirkjan.java
+++ src/aswetty/dirkjan.java
@@ -1,5 +1,17 @@
 package aswetty;
 
 public class dirkjan {
-	int dirkjanhaan;
+	int dirkjan;
+	String haan;
+	
+	dirkjan(String haan){
+		this.haan = haan;
+		
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "dirkjan[haan: "+ haan +" ," + dirkjan + "]";
+	}
 }
